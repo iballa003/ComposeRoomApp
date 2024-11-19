cf3be2d0e33c6962e9452c26ec9acce9189f42e2
iballa003 <iballa.montero@gmail.com>
2024-11-19 12:13:47 +0000

    Terminado el MainActivity.kt con el composable.

--- app/src/main/java/org/iesharia/composeroomapp/AppDatabase.kt
+++ app/src/main/java/org/iesharia/composeroomapp/AppDatabase.kt
@@ -1,6 +1,7 @@
 package org.iesharia.composeroomapp
 
 import android.content.Context
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,21 +9,13 @@ import androidx.room.RoomDatabase
 @Database(entities = [Task::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao():
-            TaskDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-        fun getDatabase(context: Context):
-                AppDatabase {
-                    return INSTANCE ?: synchronized(this) {
-                        val instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        AppDatabase::class.java,
-                        "task_database"
-                        ).build()
-                INSTANCE = instance instance
+            TaskDao companion object {
+            @Volatile private var INSTANCE: AppDatabase? = null
+            fun getDatabase(context: Context):
+                    AppDatabase { return INSTANCE ?: synchronized(this) {
+                        val instance = Room.databaseBuilder( context.applicationContext, AppDatabase::class.java, "task_database" ).build()
+                        INSTANCE = instance instance
+                    }
             }
-        }
-    }
+            }
 }
--- app/src/main/java/org/iesharia/composeroomapp/MainActivity.kt
+++ app/src/main/java/org/iesharia/composeroomapp/MainActivity.kt
@@ -4,44 +4,62 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.iesharia.composeroomapp.ui.theme.ComposeRoomAppTheme
+import androidx.compose.material3.OutlinedTextField
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private lateinit var database: AppDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeRoomAppTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TaskApp(database)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeRoomAppTheme {
-        Greeting("Android")
+fun TaskApp(database: AppDatabase) {
+    val taskDao = database.taskDao()
+    val scope = rememberCoroutineScope()
+    var tasks by remember { mutableStateOf(listOf<Task>()) }
+    var newTaskName by remember { mutableStateOf("") }
+    LaunchedEffect(Unit) {
+        tasks = taskDao.getAllTasks()
     }
+    Column( modifier = Modifier .fillMaxSize() .padding(16.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally ){
+        OutlinedTextField(value = newTaskName, onValueChange = { newTaskName = it }, label = { Text("New Task") }, modifier = Modifier.fillMaxWidth())
+        Button(onClick = {
+            scope.launch(Dispatchers.IO) {
+                val newTask = Task(name = newTaskName)
+                taskDao.insert(newTask)
+                tasks = taskDao.getAllTasks()
+                newTaskName = ""
+            }
+
+        }) {Text("Add Task") }
+        tasks.forEach { task -> Text(text = task.name) }
+    }
+
 }
